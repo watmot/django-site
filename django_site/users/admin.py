938d37e8536f3161528154ea165be546ab500d99
watmot <tomwatson840@gmail.com>
2020-03-09 22:01:55 +0000

    part 6 - added form but no functionality

--- django_site/users/admin.py
+++ django_site/users/admin.py
@@ -1,2 +1,3 @@
 from django.contrib import admin
-from blog.
+
+# Register your models here.
---
+++ django_site/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/register.html', context=context)
