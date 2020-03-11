412cc1995bc9cbc0a44fded17fcee17cc325499e
watmot <tomwatson840@gmail.com>
2020-03-11 19:05:44 +0000

    part 7 - complete

--- django_site/django_site/urls.py
+++ django_site/django_site/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='log-in'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='log-out'),
+    path('profile/', users_views.profile, name='profile'),
 ]
